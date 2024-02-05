434cb8e3c3f1a4d3eb3dc22b24dc9ffe04b377f1
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-05 15:49:47 -0300

    Update index_script.js

--- material/scripts/index_script.js
+++ material/scripts/index_script.js
@@ -1,1 +1,9 @@
-
+$(".menu").on("click", function () {
+  if ($(".l-site").hasClass("is-open")) {
+    $(".menu").removeClass("is-active");
+    $(".l-site").removeClass("is-open");
+  } else {
+    $(".menu").addClass("is-active");
+    $(".l-site").addClass("is-open");
+  }
+});
