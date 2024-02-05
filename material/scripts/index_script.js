b590a4a72ce5890bf335a0ce5d3d01fcd649f54b
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-05 15:56:50 -0300

    Update index_script.js

--- material/scripts/index_script.js
+++ material/scripts/index_script.js
@@ -1,9 +1,11 @@
-$(".menu").on("click", function () {
-  if ($(".l-site").hasClass("is-open")) {
-    $(".menu").removeClass("is-active");
-    $(".l-site").removeClass("is-open");
-  } else {
-    $(".menu").addClass("is-active");
-    $(".l-site").addClass("is-open");
-  }
+$(document).ready(function(){
+  $(".menu").on("click", function () {
+    if ($(".l-site").hasClass("is-open")) {
+      $(".menu").removeClass("is-active");
+      $(".l-site").removeClass("is-open");
+    } else {
+      $(".menu").addClass("is-active");
+      $(".l-site").addClass("is-open");
+    }
+  });
 });
